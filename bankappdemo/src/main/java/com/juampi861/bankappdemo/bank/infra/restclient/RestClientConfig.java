86686689b1980b527e5b1e8c190ff877090c1343
Juan Diaz <diazjuampi@gmail.com>
2025-05-23 09:01:46 -0300

    fixing auth for rest client

--- bankappdemo/src/main/java/com/juampi861/bankappdemo/bank/infra/restclient/RestClientConfig.java
+++ bankappdemo/src/main/java/com/juampi861/bankappdemo/bank/infra/restclient/RestClientConfig.java
@@ -1,12 +1,23 @@
 package com.juampi861.bankappdemo.bank.infra.restclient;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestClientConfig {
+
+    @Value("${bank-app.username}")
+    private String username;
+
+    @Value("${bank-app.password}")
+    private String password;
+
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplateBuilder()
+                .basicAuthentication(username, password)
+                .build();
     }
 }
